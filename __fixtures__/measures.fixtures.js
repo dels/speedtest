9979ba1edb8a884448b1665d69017bc9f4cffc88
Dominik Elsbroek <de@elsbroek.com>
2021-12-27 12:09:42 +0100

    working on bug fixes

--- __fixtures__/measures.fixtures.js
+++ __fixtures__/measures.fixtures.js
@@ -156,3 +156,44 @@ export function fiveConsequtiveMeasures(){
         }
     }
 }
+
+
+export function fourConsequtiveMeasures(){
+    return {
+        measures: {
+            20211222: {
+                1032: {
+                    download: 247154746.90951532,
+                    upload: 28194124.543322228,
+                    timestamp: "2021-12-22T09:32:02.190208Z",
+                    download_mbit: 235.70513430549175,
+                    upload_mbit: 26.8880124505255
+                },
+                1106: {
+                    download: 187943630.89514783,
+                    upload: 27618109.151399244,
+                    ping: 39.976,
+                    timestamp: "2021-12-22T10:06:02.117510Z",
+                    download_mbit: 179.23701371683867,
+                    upload_mbit: 26.33868136539387
+                },
+                1206: {
+                    download: 187943630.89514783,
+                    upload: 27618109.151399244,
+                    ping: 39.976,
+                    timestamp: "2021-12-22T10:06:02.117510Z",
+                    download_mbit: 179.23701371683867,
+                    upload_mbit: 26.33868136539387
+                },
+                1306: {
+                    download: 187943630.89514783,
+                    upload: 27618109.151399244,
+                    ping: 39.976,
+                    timestamp: "2021-12-22T10:06:02.117510Z",
+                    download_mbit: 179.23701371683867,
+                    upload_mbit: 26.33868136539387
+                }
+            }
+        }
+    }
+}
